0c277680ef894a8f1d7917e897d0644e6296d504
thefedorable <116910060+thefedorable@users.noreply.github.com>
2025-03-10 13:59:01 -0600

    Add park images

--- application/src/components/ParkDisplay/RideCard.tsx
+++ application/src/components/ParkDisplay/RideCard.tsx
@@ -5,7 +5,7 @@ import {
     CardTitle,
   } from "@/components/ui/card"
   import Image from "next/image";
-  import Big from "../../../public/ParksIcons/Big_Thunder_Moutain_Railroad.jpg";
+  import Big from "../../../public/ParksIcons/big-thunder-mountain-railroad-00.avif";
 import { Ride } from "@/lib/actions/Ride";
 import { ellipsis } from "@/lib/utils";
   
@@ -36,21 +36,23 @@ import { ellipsis } from "@/lib/utils";
     return (
       <div className="flex flex-row justify-center w-full p-2">
         <Card className="w-full max-w-[325px] flex flex-row overflow-hidden">
-          {/* Image on the left side */}
-          <div className="w-1/4 sm:w-1/3 bg-gray-200">
-            <Image 
-              src={Big.src}
-              alt={ride.getName()} 
-              width={100}
-              height={100}
-              className="h-full w-full object-cover"
-            />
+          {/* Image container with 16:9 aspect ratio on the left side */}
+          <div className="w-1/4 sm:w-1/3 relative">
+            <div className="aspect-w-16 aspect-h-9 h-full">
+              <Image 
+                src={Big.src}
+                alt={ride.getName()} 
+                width={160}
+                height={90}
+                className="object-cover absolute inset-0 w-full h-full"
+              />
+            </div>
           </div>
           
           {/* Card content on the right side */}
-          <div className="w-3/4">
+          <div className="w-3/4 sm:w-2/3">
             <CardHeader className="p-2">
-              <CardTitle className="text-xl">{ellipsis(ride.getName(), 40)} </CardTitle>
+              <CardTitle className="text-xl">{ellipsis(ride.getName(), 40)}</CardTitle>
             </CardHeader>
             <CardContent className="p-1">
               <p className="text-2xl">{rideTime()}</p>
